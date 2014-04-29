db5e2dde8aa25ccb3893ef4f835b90fafa93610d
Alex Rønne Petersen <alexrp@xamarin.com>
2014-04-30 00:15:13 +0200

    Don't print IL offsets when stringizing stack frames without source.
    The offset is already present in the stack frame header.

--- src/Utilities.cs
+++ src/Utilities.cs
@@ -98,7 +98,7 @@ namespace Mono.Debugger.Client
 
                 if (line != null && !line.IsOutOfRange)
                 {
-                    src = string.Format("0x{0:X8}    {1}", line.Address, line.Code);
+                    src = string.Format("    {0}", line.Code);
                     tag = " (no source)";
                 }
             }

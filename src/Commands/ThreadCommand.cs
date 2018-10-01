c26ee8fbeb87994227ff1a2e92cb36d6748965f0
Alex Rønne Petersen <alex@alexrp.com>
2018-10-01 07:58:55 +0200

    Update copyright year.

--- src/Commands/ThreadCommand.cs
+++ src/Commands/ThreadCommand.cs
@@ -1,7 +1,7 @@
 //
 // The MIT License (MIT)
 //
-// Copyright (c) 2015 Alex Rønne Petersen
+// Copyright (c) 2018 Alex Rønne Petersen
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
